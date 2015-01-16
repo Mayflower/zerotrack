b2cfa07fe3b0552231399ed01f70fe9ca9435ad5
codec <codec@fnord.cx>
2015-01-16 20:06:19 +0100

    keep 10 last messages per group ... "backlog"

--- 0track.rb
+++ 0track.rb
@@ -17,10 +17,18 @@ class ZerotrackConnection < EM::WebSocket::Connection
     @pubsub.on :message do |group, data|
       send data
     end
+
+    @redis.lrange("messages:#{@group}", 0,4) do |backlog|
+      backlog.each do |data|
+        send data
+      end
+    end
   end
 
   def trigger_on_message(data)
     @redis.publish @group, data
+    @redis.lpush("messages:#{@group}", data)
+    @redis.ltrim("messages:#{@group}", 0, 9)
   end
 
   def trigger_on_close(event = {})
