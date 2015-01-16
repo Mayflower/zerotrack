bd36e7692535c5477058c04d24d8f9b303915508
codec <codec@fnord.cx>
2015-01-16 18:57:28 +0100

    use the bootleaf template

---
+++ assets/js/app.js
@@ -0,0 +1,121 @@
+var map, featureList;
+
+$(document).on("click", ".feature-row", function(e) {
+  sidebarClick(parseInt($(this).attr("id"), 10));
+});
+
+$("#about-btn").click(function() {
+  $("#aboutModal").modal("show");
+  $(".navbar-collapse.in").collapse("hide");
+  return false;
+});
+
+$("#login-btn").click(function() {
+  $("#loginModal").modal("show");
+  $(".navbar-collapse.in").collapse("hide");
+  return false;
+});
+
+$("#list-btn").click(function() {
+  $('#sidebar').toggle();
+  map.invalidateSize();
+  return false;
+});
+
+$("#nav-btn").click(function() {
+  $(".navbar-collapse").collapse("toggle");
+  return false;
+});
+
+$("#sidebar-toggle-btn").click(function() {
+  $("#sidebar").toggle();
+  map.invalidateSize();
+  return false;
+});
+
+$("#sidebar-hide-btn").click(function() {
+  $('#sidebar').hide();
+  map.invalidateSize();
+});
+
+function sidebarClick(id) {
+  // map.addLayer(theaterLayer).addLayer(museumLayer);
+  var layer = markerClusters.getLayer(id);
+  map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 17);
+  layer.fire("click");
+  /* Hide sidebar and go to the map on small screens */
+  if (document.body.clientWidth <= 767) {
+    $("#sidebar").hide();
+    map.invalidateSize();
+  }
+}
+
+/* Basemap Layers */
+var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: 'Map &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+});
+/* Overlay Layers */
+var highlight = L.geoJson(null);
+
+map = L.map("map", {
+  zoom: 10,
+  layers: [osm],
+  zoomControl: false
+});
+
+map.locate({setView: true});
+
+/* Clear feature highlight when map is clicked */
+map.on("click", function(e) {
+  highlight.clearLayers();
+});
+
+var zoomControl = L.control.zoom({
+  position: "bottomright"
+}).addTo(map);
+
+/* GPS enabled geolocation control set to follow the user's location */
+var locateControl = L.control.locate({
+  position: "bottomright",
+  drawCircle: false,
+  drawMarker: false,
+  follow: true,
+  setView: true,
+  keepCurrentZoomLevel: true,
+  icon: "icon-direction",
+  metric: false,
+  strings: {
+    title: "My location",
+    popup: "You are within {distance} {unit} from this point",
+    outsideMapBoundsMsg: "You seem located outside the boundaries of the map"
+  },
+  locateOptions: {
+    maxZoom: 18,
+    watch: true,
+    enableHighAccuracy: true,
+    maximumAge: 10000,
+    timeout: 10000
+  }
+}).addTo(map);
+
+/* Larger screens get expanded layer control and visible sidebar */
+if (document.body.clientWidth <= 767) {
+  var isCollapsed = true;
+} else {
+  var isCollapsed = false;
+}
+
+var baseLayers = {
+  "Street Map": osm
+};
+
+function syncSidebar() {
+  osm.eachLayer(function (layer) {
+      if (map.hasLayer(osm)) {
+        if (map.getBounds().contains(layer.getLatLng())) {
+          $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/theater.png"></td><td class="feature-name">' + layer.feature.properties.NAME + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
+        }
+      }
+    });
+};
--- assets/js/zerotrack.js
+++ assets/js/zerotrack.js
@@ -1,34 +1,34 @@
 sjcl.random.startCollectors();
 
-
 var Zerotrack;
 
 Zerotrack = (function(){
-  function Zerotrack() {
+  function Zerotrack(map) {
     this.group = window.location.pathname.substring(1);
     this.username = Math.random().toString(36).substring(7);
     this.socket = new WebSocket('ws://localhost:9393/' + this.group);
     this.markers = {};
-    this.map = L.map('map', { zoomControl: false }).locate({setView: true, maxZoom: 18});
-
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-      maxZoom: 18,
-    }).addTo(this.map);
-
-    L.control.zoom({
-      position: 'topright',
-    }).addTo(this.map);
+    this.map = map;
 
     this.socket.onmessage = function(e) { this.processMessage(e); }.bind(this);
     this.socket.onerror = function(e) { console.error('Error: ' + e); };
     this.socket.onclose = function(e) { console.error('Connection closed'); };
+
+    if (this.groupKey() == "") {
+      window.location.href = "#" + Math.random().toString(36).substring(7);
+    };
   };
+
 
   Zerotrack.prototype.processMessage = function(event) {
     var msg = JSON.parse(this.decrypt(this.groupKey(), event.data));
     console.log(msg);
-    this.setMarker(msg);
+
+    if (msg.user == this.username) {
+      console.log("received update from self");
+    } else {
+      this.setMarker(msg);
+    }
   };
 
   Zerotrack.prototype.sendMessage = function(msg) {
@@ -37,9 +37,10 @@ Zerotrack = (function(){
 
   Zerotrack.prototype.setMarker = function(data) {
     if (typeof(this.markers[data.user]) === 'undefined') {
-      marker = new L.marker([data.lat, data.lon]).addTo(this.map);
+      marker = new L.marker([data.lat, data.lon], 100).addTo(this.map);
       marker.bindPopup("<strong>" + data.user + "</strong>");
       this.markers[data.user] = marker;
+      $("#feature-list tbody").append('<tr class="feature-row" id="' + data.user + '" lat="' + data.lat + '" lng="' + data.lon + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/theater.png"></td><td class="feature-name">' + data.user + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>')
     } else {
       this.markers[data.user].setLatLng([data.lat, data.lon]).update();
     }
@@ -48,7 +49,7 @@ Zerotrack = (function(){
   Zerotrack.prototype.sendLocation = function(event) {
     navigator.geolocation.getCurrentPosition(
       function success(event) {
-        this.sendMessage({'user': this.username, 'lat': event.coords.latitude, 'lon': event.coords.longitude});
+        this.sendMessage({'user': this.username, 'lat': event.coords.latitude, 'lon': event.coords.longitude, 'acc': event.coords.accuracy});
       }.bind(this),
       function error(error) {
         alert(error);
@@ -69,7 +70,7 @@ Zerotrack = (function(){
     var key = window.location.hash.substring(1);
     i = key.indexOf('='); if (i>-1) { key = key.substring(0,i+1); };
     i = key.indexOf('&'); if (i>-1) { key = key.substring(0,i); };
-    if (key.charAt(key.length-1)!=='=') key+='=';
+    // if (key.charAt(key.length-1)!=='=') key+='=';
 
     return key;
   };
@@ -77,4 +78,4 @@ Zerotrack = (function(){
   return Zerotrack;
 })();
 
-var zerotrack = new Zerotrack();
+var zerotrack = new Zerotrack(map);
